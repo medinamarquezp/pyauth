d64a403c0ea710b24f8e1d0a809f869029ff4d01
Pedro Medina Márquez <medinamarquezp@gmail.com>
2024-08-12 00:52:14 +0200

    refactor: user name should be nullable

--- src/modules/user/models/user_model.py
+++ src/modules/user/models/user_model.py
@@ -5,7 +5,7 @@ from src.modules.shared.sql import BaseModel
 class UserModel(BaseModel):
     __tablename__ = 'users'
 
-    name = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=True)
     last_name = Column(String(100), nullable=True)
     email = Column(String(120), unique=True, nullable=False)
     role = Column(Enum('admin', 'user'), default='user')
