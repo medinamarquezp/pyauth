f181263822aa9e944450c6be0b3c108fcb3809b6
Pedro Medina Márquez <medinamarquezp@gmail.com>
2024-08-11 23:31:45 +0200

    feat: get and update password

--- src/modules/auth/services/password_service.py
+++ src/modules/auth/services/password_service.py
@@ -1,11 +1,15 @@
 import bcrypt
 from src.modules.shared.services import logger
+from src.modules.auth.models import PasswordModel
 from src.modules.auth.repositories import PasswordRepository
 
 
 class PasswordService:
     def __init__(self, password_repository: PasswordRepository):
         self.password_repository = password_repository
+
+    def get_password(self, user_id: str) -> PasswordModel:
+        return self.password_repository.get_by_id(user_id)
 
     def create(self, user_id: str, password: str):
         try:
@@ -24,4 +28,24 @@ class PasswordService:
             return self.password_repository.create(data)
         except Exception as e:
             logger.error(f"Error creating password for user {user_id}: {e}")
+            raise e
+
+    def update(self, user_id: str, new_password: str):
+        try:
+            logger.info(f"Updating password for user {user_id}")
+            password = self.get_password(user_id)
+            if not password:
+                raise Exception("Password not found")
+            salt = password.salt.encode()
+            logger.info("Salt encoded")
+            hashed_password = bcrypt.hashpw(
+                new_password.encode(), salt
+            ).decode()
+            logger.info("Password hashed")
+            data = {
+                "password": hashed_password,
+            }
+            return self.password_repository.update(user_id, data)
+        except Exception as e:
+            logger.error(f"Error updating password for user {user_id}: {e}")
             raise e
