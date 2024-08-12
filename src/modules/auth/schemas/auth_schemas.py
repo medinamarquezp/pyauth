909683cc6e11a05c1fd9bdd20843001455e9d21a
Pedro Medina Márquez <medinamarquezp@gmail.com>
2024-08-12 23:17:47 +0200

    feat: signup schema improvements

--- src/modules/auth/schemas/auth_schemas.py
+++ src/modules/auth/schemas/auth_schemas.py
@@ -3,8 +3,8 @@ signup_schema = {
     "properties": {
         "name": {"type": "string"},
         "last_name": {"type": "string"},
-        "email": {"type": "string"},
-        "password": {"type": "string"},
+        "email": {"type": "string", "format": "email"},
+        "password": {"type": "string", "minLength": 8, "pattern": "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$"},
     },
     "required": ["email", "password"],
 }
