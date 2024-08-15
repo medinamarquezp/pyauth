0ff840105745de530f3db4792ac30a6f8a5bbb2b
Pedro Medina Márquez <medinamarquezp@gmail.com>
2024-08-15 22:06:36 +0200

    refactor: di ajustments

--- src/modules/shared/di/di.py
+++ src/modules/shared/di/di.py
@@ -1,22 +1,20 @@
 from src.config.app import EMAIL_SMTP
-from src.modules.user.repositories import UserRepository
-from src.modules.auth.repositories import PasswordRepository
-from src.modules.auth.repositories import VerificationTokenRepository
-from src.modules.user.services import UserService
-from src.modules.auth.services import AuthService
-from src.modules.auth.services import PasswordService
 from src.modules.shared.sql import DatabaseManager
+from src.modules.user.repositories import UserRepository
+from src.modules.auth.repositories import PasswordRepository, VerificationTokenRepository, SessionRepository
+from src.modules.user.services import UserService
 from src.modules.shared.services import EmailService
-from src.modules.auth.services import VerificationTokenService
+from src.modules.auth.services import AuthService, PasswordService,VerificationTokenService, SessionService
 
 session = DatabaseManager().get_session()
 user_repository = UserRepository(session)
+session_repository = SessionRepository(session)
 password_repository = PasswordRepository(session)
 verification_token_repository = VerificationTokenRepository(session)
 user_service = UserService(user_repository)
+session_service = SessionService(session_repository)
 password_service = PasswordService(password_repository)
-verification_token_service = VerificationTokenService(
-    verification_token_repository)
+verification_token_service = VerificationTokenService(verification_token_repository)
 email_service = EmailService(**EMAIL_SMTP)
-auth_service = AuthService(user_service, email_service,
+auth_service = AuthService(user_service, email_service, session_service,
                            password_service, verification_token_service)
